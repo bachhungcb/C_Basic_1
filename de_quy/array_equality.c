71da6ffd3ee79a58cdb56d2f23925db12592002c
bachhungcb <bachhungcb@gmail.com>
2023-10-24 15:47:38 +0700

    create array_equality

--- de_quy/array_equality.c
+++ de_quy/array_equality.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int ket_qua[1000];
 int main(){
-    int a[100];
-    int b[100];
+    int test_case;
+    int a[1000];
+    int b[1000];
     int n, m;
-    printf("Enter the size of array 1: ");
-    scanf("%d", &n);
-    printf("Enter the size of array 2: ");
-    scanf("%d", &m);
-    if(n != m){
-        printf("0");
-    }else{
-        int count = 0;
-        for (int i = 0; i < n; i++){
-            scanf("%d", &a[i]);
-        }for (int i = 0; i < m; i++){
-            scanf("%d", &b[i]);
-        }
-        for (int i = 0; i < n; i++){
-            if (a[i] != b[i]){
-                count++;
+    scanf("%d", &test_case);
+    for (int z = 0; z < test_case; z++){
+        scanf("%d", &n);
+        scanf("%d", &m);
+        if(n != m){
+            ket_qua[z] = 0;
+        }else{
+            int count = 0;
+            for (int i = 0; i < n; i++){
+                scanf("%d ", &a[i]);
+            }for (int i = 0; i < m; i++){
+                scanf("%d ", &b[i]);
             }
+            for (int i = 0; i < n; i++){
+                if (a[i] != b[i]){
+                    count++;
+                }
+            }
+            if (count != 0) ket_qua[z] = 0;
+            else ket_qua[z] = 1;
         }
-        if (count != 0) printf("0");
-        else printf("1");
+    }
+
+    for (int z = 0; z < test_case; z++){
+        printf("%d \n", ket_qua[z]);
     }
     
+    return 0;
 }
