6fd522107c235b5608e249b2b914596c87f82889
bachhungcb <bachhungcb@gmail.com>
2023-10-30 17:07:46 +0700

    update sudoku_allsolution

--- de_quy/sudoku_all_solution.c
+++ de_quy/sudoku_all_solution.c
@@ -1,122 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-// N is the size of the 2D matrix N*N
 #define N 9
 
-/* A utility function to print grid */
-void print(int arr[N][N])
-{
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-			printf("%d ",arr[i][j]);
+void print_solution (int arr[N][N]){
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			printf("%d ", arr[i][j]);
+		}
 		printf("\n");
 	}
 }
 
-// Checks whether it will be legal 
-// to assign num to the
-// given row, col
-int isSafe(int grid[N][N], int row, int col, int num)
-{
-	
-	// Check if we find the same num 
-	// in the similar row , we return 0
-	for (int x = 0; x <= 8; x++)
-		if (grid[row][x] == num)
-			return 0;
+int is_safe(int arr[N][N], int hang, int cot, int num){
+	//kiem tra xem cac so co cung mot hang hay khong
+	for (int i  = 0; i <= 8; i++){
+		if(arr[hang][i] == num) return 0;
+	}
 
-	// Check if we find the same num in the 
-	// similar column , we return 0
-	for (int x = 0; x <= 8; x++)
-		if (grid[x][col] == num)
-			return 0;
+	//kiem tra xem cac so co cung mot cot hay khong
+	for(int i = 0; i <= 8; i++){
+		if(arr[i][cot] == num) return 0;
+	}
 
-	// Check if we find the same num in the 
-	// particular 3*3 matrix, we return 0
-	int startRow = row - row % 3, startCol = col - col % 3;
+	//kiem tra cac ma tran 3x3
+	int hang_bat_dau = hang - hang % 3;
+	int cot_bat_dau = cot - cot % 3;
 
-	for (int i = 0; i < 3; i++)
-		for (int j = 0; j < 3; j++)
-			if (grid[i + startRow][j + startCol] == num)
-				return 0;
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			if(arr[i + hang_bat_dau][j + cot_bat_dau] == num) return 0;
+		}
+	}
 
 	return 1;
 }
 
-/* Takes a partially filled-in grid and attempts
-to assign values to all unassigned locations in
-such a way to meet the requirements for
-Sudoku solution (non-duplication across rows,
-columns, and boxes) */
-int solveSudoku(int grid[N][N], int row, int col)
-{
-	
-	// Check if we have reached the 8th row 
-	// and 9th column (0
-	// indexed matrix) , we are 
-	// returning true to avoid
-	// further backtracking
-	if (row == N - 1 && col == N)
+int solve_sudoku(int arr[N][N], int hang, int cot){
+	if (hang == N - 1 && cot == N)
 		return 1;
+	
 
-	// Check if column value becomes 9 ,
-	// we move to next row and
-	// column start from 0
-	if (col == N) 
-	{
-		row++;
-		col = 0;
+	if (cot == N){
+		hang++;
+		cot = 0;
 	}
 
-	// Check if the current position 
-	// of the grid already contains
-	// value > 0, we iterate for next column
-	if (grid[row][col] > 0)
-		return solveSudoku(grid, row, col + 1);
+	if(arr[hang][cot] > 0)
+		return solve_sudoku(arr, hang, cot + 1);
+	
+	for(int num = 1; num <= N; num++){
+		if (is_safe(arr, hang, cot, num) == 1){
+			arr[hang][cot] = num;
 
-	for (int num = 1; num <= N; num++) 
-	{
-		
-		// Check if it is safe to place 
-		// the num (1-9) in the
-		// given row ,col ->we move to next column
-		if (isSafe(grid, row, col, num)==1) 
-		{
-			/* assigning the num in the 
-			current (row,col)
-			position of the grid
-			and assuming our assigned num 
-			in the position
-			is correct	 */
-			grid[row][col] = num;
-		
-			// Checking for next possibility with next
-			// column
-			if (solveSudoku(grid, row, col + 1)==1)
+			if(solve_sudoku(arr, hang, cot + 1) == 1) 
 				return 1;
 		}
-	
-		// Removing the assigned num ,
-		// since our assumption
-		// was wrong , and we go for next 
-		// assumption with
-		// diff num value
-		grid[row][col] = 0;
+
+		arr[hang][cot] = 0;
 	}
 	return 0;
 }
 
 int main()
 {
-    srand(time(0));
 	// 0 means unassigned cells
 	int grid[N][N] = { 
 					{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 					{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-					{ 0, 0, 0, 0, rand() % 9, 0, 0, 0, 0 },
+					{ 0, 0, 0, 0, 7, 0, 0, 0, 0 },
 					{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 					{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 					{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -125,13 +77,10 @@ int main()
 					{ 0, 0, 0, 0, 0, 0, 0, 0, 0 } 
 					};
 
-    printf("De bai:\n");
-    print(grid);
-    printf("Dap an:\n");
-
-	if (solveSudoku(grid, 0, 0)==1)
-		print(grid);
+	if (solve_sudoku(grid, 0, 0)==1)
+		print_solution(grid);
 	else
 		printf("No solution exists");
+
 	return 0;
 }
