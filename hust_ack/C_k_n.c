2def90cfbf189d7c4aa8f62a3690c3d6c07f8441
bachhungcb <bachhungcb@gmail.com>
2023-12-10 14:31:48 +0700

    solve Ckn problem with 10 modulo 7 precision

--- hust_ack/C_k_n.c
+++ hust_ack/C_k_n.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int M[100][100] = {0};
+#define MOD 1000000007
+#define MAX 1000
 
-int Ckn(int k, int n){
-    if(k == 0|| k == n) return M[k][n] = 1;
-    else if(M[k][n] == 0){
-        M[k][n] = Ckn(k-1,n-1) + Ckn(k, n-1);
+unsigned long long M[MAX][MAX];
+
+unsigned long long binomialCoefficient(int k, int n) {
+    if (k == 0 || k == n) return M[k][n] = 1;
+    else if (M[k][n] == 0) {
+        M[k][n] = (binomialCoefficient(k-1, n-1) + binomialCoefficient(k, n-1)) % MOD;
     }
     return M[k][n];
 }
 
-int main(){
-    int n,k;
+int main() {
+    int n, k;
+    memset(M, 0, sizeof(M));
+
     scanf("%d %d", &k, &n);
-    printf("%d", Ckn(k,n));
+
+    unsigned long long result = binomialCoefficient(k, n);
+    printf("%llu", result);
+
     return 0;
 }
