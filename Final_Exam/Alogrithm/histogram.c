23a89fe937d4722c0bf0c8b92cda4aacd6c2b020
bachhungcb <bachhungcb@gmail.com>
2024-01-19 00:15:51 +0700

    update some file

---
+++ Final_Exam/Alogrithm/histogram.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <limits.h>
+
+int main(){
+    int n, cnt;
+    int maxCnt = 0;
+    int minLength = INT_MAX;
+    scanf("%d", &n);
+    int histogram[n];
+
+    for(int i = 0; i < n; i++){
+        scanf("%d", &histogram[i]);
+    }
+
+     for(int i = 0; i < n; i++){
+        
+        if(abs(histogram[i-1] - histogram[i]) <=1){
+            cnt++;
+
+            if(cnt > maxCnt){
+                maxCnt = cnt;
+            }
+            
+            if(histogram[i] < minLength)
+                minLength = histogram[i];
+
+        }else{
+            cnt = 0;
+        }
+    }
+
+    printf("%d\n", (maxCnt + 1) * minLength);
+    //printf("%d\n", maxCnt);
+    //printf("%d\n", minLength);
+
+    return 0;
+}
