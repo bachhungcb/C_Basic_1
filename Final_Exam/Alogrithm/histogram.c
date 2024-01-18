b553bf48e600250420e87f9bd5bd19db1b47faf9
bachhungcb <bachhungcb@gmail.com>
2024-01-19 00:36:20 +0700

    Update histogram.c

--- Final_Exam/Alogrithm/histogram.c
+++ Final_Exam/Alogrithm/histogram.c
@@ -1,39 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <limits.h>
 
-int main(){
-    int n, cnt;
-    int maxCnt = 0;
-    int minLength = INT_MAX;
-    scanf("%d", &n);
-    int histogram[n];
+int maxArea(int* histogram, int n) {
+    int* stack = (int*)malloc(n * sizeof(int));
+    int top = -1;
+    int maxArea = 0;
+    int areaWithTop;
+    int i = 0;
 
-    for(int i = 0; i < n; i++){
-        scanf("%d", &histogram[i]);
-    }
+    while (i < n) {
+        if (top == -1 || histogram[stack[top]] <= histogram[i])
+            stack[++top] = i++;
+        else {
+            int tp = stack[top--];
+            areaWithTop = histogram[tp] * (top == -1 ? i : i - stack[top] - 1);
 
-     for(int i = 1; i <=n; i++){
-        
-        if(abs(histogram[i-1] - histogram[i]) <=1){
-            cnt++;
-            
-            if(cnt > maxCnt){
-                maxCnt = cnt;
-            }
-            
-            if(histogram[i-1] < minLength)
-                minLength = histogram[i-1];
-
-        }else{
-            cnt = 0;
+            if (maxArea < areaWithTop)
+                maxArea = areaWithTop;
         }
     }
 
-    printf("\n%d\n", (maxCnt + 1) * minLength);
-    //printf("%d\n", maxCnt);
-    //printf("%d\n", minLength);
+    while (top != -1) {
+        int tp = stack[top--];
+        areaWithTop = histogram[tp] * (top == -1 ? i : i - stack[top] - 1);
 
+        if (maxArea < areaWithTop)
+            maxArea = areaWithTop;
+    }
+
+    free(stack);
+    return maxArea;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int* histogram = (int*)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &histogram[i]);
+
+    printf("%d\n", maxArea(histogram, n));
+    free(histogram);
     return 0;
 }
